084b6b7f2922681413503ddc33fb4a13472bbe70
John Clark <inindev@gmail.com>
2021-06-23 10:15:02 -0400

    use mmc1 as default

--- uboot/make_uboot.sh
+++ uboot/make_uboot.sh
@@ -9,13 +9,26 @@ main() {
         git -C u-boot fetch --tags
         git -C u-boot checkout -b uboot-2021.04 v2021.04
         git -C u-boot cherry-pick b69b9f3f54732c303939eb748aad97cd4cf60168
+
+        # remove video support
+        sed -i \
+          -e '/^CONFIG_DM_VIDEO=y$/d' \
+          -e '/^CONFIG_DISPLAY=y$/d' \
+          -e '/^CONFIG_VIDEO_ROCKCHIP=y$/d' \
+          -e '/^CONFIG_DISPLAY_ROCKCHIP_HDMI=y$/d' \
+          u-boot/configs/nanopi-r4s-rk3399_defconfig
+
+        # set default mmc to 1
+        sed -i 's/^CONFIG_ROCKCHIP_GPIO=y$/CONFIG_SYS_MMC_ENV_DEV=1\n&/' u-boot/configs/nanopi-r4s-rk3399_defconfig
     fi
 
     if [ ! -f u-boot/rk3399_bl31.elf ]; then
         wget -c https://github.com/atf-builds/atf/releases/download/v2.4/atf-v2.4.tar.gz -O - | tar -C u-boot -xz ./rk3399_bl31.elf
     fi
 
-    git -C u-boot checkout uboot-2021.04
+    if [ "uboot-2021.04" != "$(git -C u-boot branch | sed -n -e 's/^\* \(.*\)/\1/p')" ]; then
+        git -C u-boot checkout uboot-2021.04
+    fi
 
     make -C u-boot mrproper
     make -C u-boot nanopi-r4s-rk3399_defconfig
