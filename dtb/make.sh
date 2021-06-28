5ca59e51686c418b719b1be6e79159f1df567a15
John Clark <inindev@gmail.com>
2021-06-28 08:12:57 -0400

    bump linux version to 5.13

--- dtb/make.sh
+++ dtb/make.sh
@@ -4,20 +4,20 @@ set -e
 
 # prerequisites: build-essential device-tree-compiler
 # kernel.org linux version
-linux='linux-5.13-rc6'
+lv='5.13'
 
-if [ ! -d "$linux" ]; then
-    if [ ! -f "$linux.tar.gz" ]; then
-        wget "https://git.kernel.org/torvalds/t/$linux.tar.gz"
+if [ ! -d "linux-$lv" ]; then
+    if [ ! -f "linux-$lv.tar.xz" ]; then
+        wget "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$lv.tar.xz"
     fi
-    tar xzvf "$linux.tar.gz" "$linux/include" "$linux/arch/arm64/boot/dts/rockchip"
+    tar xJvf "linux-$lv.tar.xz" "linux-$lv/include" "linux-$lv/arch/arm64/boot/dts/rockchip"
 fi
 
 # patchwork
 #wget -O rk3399-nanopi-r4s.patch https://patchwork.kernel.org/project/linux-rockchip/patch/20210610091357.6780-1-cnsztl@gmail.com/raw/
-#patch -b -p1 -d "$linux" < rk3399-nanopi-r4s.patch
+#patch -b -p1 -d "linux-$lv" < rk3399-nanopi-r4s.patch
 
-nanodts="$linux/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
+nanodts="linux-$lv/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
 epgpios='ep-gpios = <\&gpio2 RK_PA4 GPIO_ACTIVE_HIGH>;'
 if ! cat "$nanodts" | grep -q "$epgpios"; then
     cp "$nanodts" "$nanodts.ori"
@@ -38,13 +38,13 @@ sed -i 's/\&i2c4 {/\&i2c2 {\
 fi
 
 if [ "$1" = 'links' ]; then
-    ln -s "$linux/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
-    ln -s "$linux/arch/arm64/boot/dts/rockchip/rk3399-nanopi4.dtsi"
-    ln -s "$linux/arch/arm64/boot/dts/rockchip/rk3399.dtsi"
-    ln -s "$linux/arch/arm64/boot/dts/rockchip/rk3399-opp.dtsi"
+    ln -s "linux-$lv/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
+    ln -s "linux-$lv/arch/arm64/boot/dts/rockchip/rk3399-nanopi4.dtsi"
+    ln -s "linux-$lv/arch/arm64/boot/dts/rockchip/rk3399.dtsi"
+    ln -s "linux-$lv/arch/arm64/boot/dts/rockchip/rk3399-opp.dtsi"
 else
     # build
-    gcc -I "$linux/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3399-nanopi-r4s-top.dts "$linux/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
+    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3399-nanopi-r4s-top.dts "linux-$lv/arch/arm64/boot/dts/rockchip/rk3399-nanopi-r4s.dts"
     dtc -O dtb -o rk3399-nanopi-r4s.dtb rk3399-nanopi-r4s-top.dts
 fi
 
