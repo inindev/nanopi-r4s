4f2a4b5a9959f472247edac5530cbb10f83dfb7b
John Clark <inindev@gmail.com>
2021-07-15 09:19:46 -0400

    enable bullseye security updates

--- debian/make_debian_img.sh
+++ debian/make_debian_img.sh
@@ -229,6 +229,7 @@ check_installed() {
 
 file_apt_sources() {
     local deb_dist="$1"
+    local deb_dist_security="$deb_dist$([ 'bullseye' = "$deb_dist" ] && echo '-security')"
 
     cat <<-EOF
 	# For information about how to configure apt package sources,
@@ -237,8 +238,8 @@ file_apt_sources() {
 	deb http://deb.debian.org/debian/ $deb_dist main
 	deb-src http://deb.debian.org/debian/ $deb_dist main
 
-	deb http://security.debian.org/debian-security/ $deb_dist/updates main
-	deb-src http://security.debian.org/debian-security/ $deb_dist/updates main
+	deb http://security.debian.org/debian-security/ $deb_dist_security/updates main
+	deb-src http://security.debian.org/debian-security/ $deb_dist_security/updates main
 
 	deb http://deb.debian.org/debian/ $deb_dist-updates main
 	deb-src http://deb.debian.org/debian/ $deb_dist-updates main
