cba152209958ce6498000cfe266f705a6e91b7fc
John Clark <inindev@gmail.com>
2021-07-15 06:45:37 -0400

    remove vcc3v3 which uses the wrong vcc5v0 supply voltage

--- uboot/make_uboot.sh
+++ uboot/make_uboot.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 if [ ! -d u-boot ]; then
     git clone https://github.com/u-boot/u-boot.git
     git -C u-boot fetch --tags
@@ -16,8 +15,9 @@ if ! git -C u-boot branch | grep -q uboot-2021.04; then
     git -C u-boot am ../patches/0003-ethaddr-eth1addr-from-eeprom.patch
     git -C u-boot am ../patches/0004-pcie_dw_rockchip-compilation-errors.patch
     git -C u-boot am ../patches/0005-enable-usb-host.patch
+    git -C u-boot am ../patches/0006-vcc3v3-uses-wrong-vcc5-supply-voltage.patch
     # optional boot from usb
-    #git -C u-boot am ../patches/0006-optional-boot-from-usb.patch
+    #git -C u-boot am ../patches/0007-optional-boot-from-usb.patch
 elif [ "uboot-2021.04" != "$(git -C u-boot branch | sed -n -e 's/^\* \(.*\)/\1/p')" ]; then
     git -C u-boot checkout uboot-2021.04
 fi
@@ -35,9 +35,9 @@ u-boot/tools/mkimage -n rk3399 -T rksd -d u-boot/tpl/u-boot-tpl.bin rksd_loader.
 cat u-boot/spl/u-boot-spl.bin >> rksd_loader.img
 cp u-boot/u-boot.itb .
 
-
 echo '\nu-boot and spl binaries are now ready'
 echo '\ncopy images to media:'
 echo '  dd bs=4K seek=8 if=rksd_loader.img of=/dev/sdX conv=notrunc'
 echo '  dd bs=4K seek=2048 if=u-boot.itb of=/dev/sdX conv=notrunc'
 echo '  sync\n'
+
