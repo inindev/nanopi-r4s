707c3e22cf89db83ccefed56197b1df73359299d
John Clark <inindev@gmail.com>
2023-03-11 13:10:43 -0500

    cleanup and simplify script
    Signed-off-by: John Clark <inindev@gmail.com>

--- dtb/make_dtb.sh
+++ dtb/make_dtb.sh
@@ -33,65 +33,25 @@ main() {
         exit 0
     fi
 
-    nanodts="$rkpath/rk3399-nanopi-r4s.dts"
-    if [ ! -f "$nanodts.ori" ]; then
-        cp "$nanodts" "$nanodts.ori"
-    fi
+    for dt in "rk3399-nanopi-r4s" "rk3399-nanopi-r4s-enterprise"; do
+        nanodts="$rkpath/${dt}.dts"
+        if [ ! -f "$nanodts.ori" ]; then
+            cp "$nanodts" "$nanodts.ori"
+        fi
 
-    nanodts_ent="$rkpath/rk3399-nanopi-r4s-enterprise.dts"
-    if [ ! -f "$nanodts_ent.ori" ]; then
-        cp "$nanodts_ent" "$nanodts_ent.ori"
-    fi
+        # lan & wan leds
+        if ! grep -q 'r8169-100:00:link' "$nanodts"; then
+            sed -i 's/label = "green:lan";/&\n\t\t\tlinux,default-trigger = "r8169-100:00:link";/' "$nanodts"
+        fi
+        if ! grep -q 'stmmac-0:01:link' "$nanodts"; then
+            sed -i 's/label = "green:wan";/&\n\t\t\tlinux,default-trigger = "stmmac-0:01:link";/' "$nanodts"
+        fi
 
-    # lan & wan leds
-    if ! grep -q 'r8169-100:00:link' "$nanodts"; then
-        sed -i 's/label = "green:lan";/&\n\t\t\tlinux,default-trigger = "r8169-100:00:link";/' "$nanodts"
-    fi
-    if ! grep -q 'stmmac-0:01:link' "$nanodts"; then
-        sed -i 's/label = "green:wan";/&\n\t\t\tlinux,default-trigger = "stmmac-0:01:link";/' "$nanodts"
-    fi
-
-    if ! grep -q 'r8169-100:00:link' "$nanodts_ent"; then
-        sed -i 's/label = "green:lan";/&\n\t\t\tlinux,default-trigger = "r8169-100:00:link";/' "$nanodts_ent"
-    fi
-    if ! grep -q 'stmmac-0:01:link' "$nanodts_ent"; then
-        sed -i 's/label = "green:wan";/&\n\t\t\tlinux,default-trigger = "stmmac-0:01:link";/' "$nanodts_ent"
-    fi
-
-    # see https://patchwork.kernel.org/project/linux-rockchip/patch/20210607081727.4723-1-cnsztl@gmail.com
-    #if ! grep -q '&i2c2' "$nanodts"; then
-    #    sed -i 's/\&i2c4 {/\&i2c2 {\
-    #	eeprom@51 {\
-    #		compatible = "microchip,24c02", "atmel,24c02";\
-    #		reg = <0x51>;\
-    #		pagesize = <16>;\
-    #		size = <256>;\
-    #		read-only;\
-    #	};\
-    #};\n\n&/' "$nanodts"
-    #fi
-
-    # rk3399dtsi="$rkpath/rk3399.dtsi"
-    # if [ ! -f "$rk3399dtsi.ori" ]; then
-    #     cp "$rk3399dtsi" "$rk3399dtsi.ori"
-    # fi
-
-    # back-out change ec3028e7c83ed03f9cd10c0373d955b489ca5ed6 on 10/07/2021
-    # this change produces the error: rockchip-pinctrl pinctrl: bank[0-4] is not valid
-    #sed -i 's/gpio0: gpio@ff720000 {/gpio0: gpio0@ff720000 {/' "$rk3399dtsi"
-    #sed -i 's/gpio1: gpio@ff730000 {/gpio1: gpio1@ff730000 {/' "$rk3399dtsi"
-    #sed -i 's/gpio2: gpio@ff780000 {/gpio2: gpio2@ff780000 {/' "$rk3399dtsi"
-    #sed -i 's/gpio3: gpio@ff788000 {/gpio3: gpio3@ff788000 {/' "$rk3399dtsi"
-    #sed -i 's/gpio4: gpio@ff790000 {/gpio4: gpio4@ff790000 {/' "$rk3399dtsi"
-
-    # build
-    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3399-nanopi-r4s-top.dts "$nanodts"
-    dtc -O dtb -o rk3399-nanopi-r4s.dtb rk3399-nanopi-r4s-top.dts
-    echo "\n${cya}success: rk3399-nanopi-r4s.dtb${rst}\n"
-
-    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3399-nanopi-r4s-enterprise-top.dts "$nanodts_ent"
-    dtc -O dtb -o rk3399-nanopi-r4s-enterprise.dtb rk3399-nanopi-r4s-enterprise-top.dts
-    echo "\n${cya}success: rk3399-nanopi-enterprise-r4s.dtb${rst}\n"
+        # build
+        gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o ${dt}-top.dts "$nanodts"
+        dtc -O dtb -o ${dt}.dtb ${dt}-top.dts
+        echo "\n${cya}success: ${dt}.dtb${rst}\n"
+    done
 }
 
 # check if utility program is installed
